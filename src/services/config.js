ad8ad4d34807a370a6729674d5f2c9d00261a474
Jvalentim <jvalentim0701@gmail.com>
2024-08-09 10:42:48 -0300

    Fix Reload Error

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const devURL = 'http://localhost:3030'
-// const prodURL = 'https://findme-backend-ten.vercel.app'
+// const devURL = 'http://localhost:3030'
+const prodURL = 'https://findme-backend-ten.vercel.app'
 
 axios.defaults.baseURL = devURL
 
